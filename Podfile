aa8d144cf4e8c6cdb343857858417d7f52fcabdb
Thanh Pham <>
2017-10-20 16:37:59 +0700

    Initial ccommit

---
+++ Podfile
@@ -0,0 +1,8 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RWPickFlavor' do
+  use_frameworks!
+  pod 'Alamofire', '~> 2.0'
+  pod 'MBProgressHUD', '~> 0.9.0'
+end
---
+++ RWPickFlavor/RWPickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  RWPickFlavor.h
+//  RWPickFlavor
+//
+//  Created by Thanh Pham on 10/20/17.
+//  Copyright © 2017 VNG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RWPickFlavor.
+FOUNDATION_EXPORT double RWPickFlavorVersionNumber;
+
+//! Project version string for RWPickFlavor.
+FOUNDATION_EXPORT const unsigned char RWPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWPickFlavor/PublicHeader.h>
+
+
